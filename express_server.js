469c66549262229db08f145c5312d1d696962c97
Taha Rizvi <riztaha@gmail.com>
2020-03-17 02:05:37 +0000

    Created a form to submit longURLs & created a random 6-digit key generator

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -27,6 +29,10 @@ app.get("/hello", (req, res) => {
   res.render("hello_world", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 //Router for specific short-long url
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
@@ -36,6 +42,22 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+//Post route to form submission
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+//Function to generate random 6-digit alpha key:
+function generateRandomString() {
+  let str = "";
+  const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  for (let i = 0; i < 6; i++) {
+    str += alpha.charAt(Math.floor(Math.random() * alpha.length));
+  }
+  return str;
+}
